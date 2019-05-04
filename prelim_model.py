ce074b6215c83be76ea02f986d7c15780d83491d
Jonathan Lin <jlin703@berkeley.edu>
2019-05-04 12:13:30 -0700

    bidirectional work

--- prelim_model.py
+++ prelim_model.py
@@ -17,20 +17,22 @@ class Model():
             tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dense(1, activation='sigmoid')
         ])
+        opt = tf.keras.optimizers.Adam(lr=self.learning_rate)
         self.model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-    def train_json(self, json_path):
+    def load_json(self, json_path):
         print("Loading data...")
         raw_data = json.load(open(json_path))
         ids = sorted(list(raw_data['id'].keys()))
         numerized, labels = raw_data['numerized'], raw_data['type']
-        train_data = np.array([numerized[id] for id in ids])
-        train_labels = np.array([1 if labels[id] == 'fake' else 0 for id in ids])
+        self.train_data = np.array([numerized[id] for id in ids])
+        self.train_labels = np.array([1 if labels[id] == 'fake' else 0 for id in ids])
 
+    def train(self):
         print("Fitting model...")
-        history = self.model.fit(train_data, train_labels, epochs=1,
+        self.history = self.model.fit(self.train_data, self.train_labels, epochs=5,
                     validation_split=0.2)
     
     def save_model(self):
